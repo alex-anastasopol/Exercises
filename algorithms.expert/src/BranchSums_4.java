281e16e1efe1820e775213d74a498ec147f1fea9
aanastasopol <aanastasopol@gmail.com>
2020-07-12 14:41:53 +0300

    Algorithm expert problem 4 is probably fixed. to test thoroughly

--- algorithms.expert/src/BranchSums_4.java
+++ algorithms.expert/src/BranchSums_4.java
@@ -2,49 +2,81 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BranchSums_4 {
-    // This is the class of the input root. Do not edit it.
-    public static class BinaryTree {
-        int value;
-        BinaryTree left;
-        BinaryTree right;
+	// This is the class of the input root. Do not edit it.
 
-        BinaryTree(int value) {
-            this.value = value;
-            this.left = null;
-            this.right = null;
-        }
-    }
+	public static class BinaryTree {
+		int value;
+		BinaryTree left;
+		BinaryTree right;
 
-    private static List<Integer> branchSums(BinaryTree root) {
-        List<Integer> sums = new ArrayList<>();
-        return calculateTreeBranchSums(0, root, sums);
-    }
+		BinaryTree(int value) {
+			this.value = value;
+			this.left = null;
+			this.right = null;
+		}
+	}
 
-    private static List<Integer> calculateTreeBranchSums(Integer currentSumIdx, BinaryTree node, List<Integer> sums) {
-        int sum = sums.get(currentSumIdx) + node.value;
-        sums.set(currentSumIdx, sum);
-        if (node.left != null) {
-            calculateTreeBranchSums(currentSumIdx + 1, node.left, sums);
-        }
+	private static List<Integer> branchSums(BinaryTree root) {
+		List<Integer> sums = new ArrayList<>();
+		sums.add(0);
+		return calculateTreeBranchSums(root, sums, 0);
+	}
 
-        if (node.right != null) {
-            calculateTreeBranchSums(currentSumIdx + 1, node.right, sums);
-        }
-        return sums;
-    }
+	private static List<Integer> calculateTreeBranchSums(BinaryTree node, List<Integer> sums, int index) {
+		sums.set(index, sums.get(index) + node.value);
+		System.out.println(
+				"idx: " + index + ", node: " + node.value + " (left: " + (node.left == null ? "null" : node.left.value)
+						+ ", right: " + (node.right == null ? "null" : node.right.value) + "): " + ", sums: " + sums);
 
-    public static void main(String[] args) {
+		int rightBaseSum = sums.get(index);
 
-        BinaryTree root = new BinaryTree(1);
-        root.left = new BinaryTree(2);
-        root.left.left = new BinaryTree(4);
-        root.left.right = new BinaryTree(5);
+		if (node.left != null) {
+			System.out.println("	will calc left branch");
+			calculateTreeBranchSums(node.left, sums, index);
+		}
 
-        root.right = new BinaryTree(3);
-        root.right.left = new BinaryTree(10);
-        root.right.right = new BinaryTree(11);
+		if (node.right != null) {
+			index = sums.size() - 1;
+			System.out.println("	will calc right branch; idx: " + index + "; sums.size():" + sums.size());
+			sums.add(rightBaseSum);
+			calculateTreeBranchSums(node.right, sums, index + 1);
+		}
 
-        List<Integer> branchSums = branchSums(root);
-        System.out.println(branchSums);
-    }
+		return sums;
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println("             1");
+		System.out.println("           /    \\");
+		System.out.println("         2        3");
+		System.out.println("        / \\      / \\");
+		System.out.println("       4   5     10  11");
+		System.out.println("      /   / \\   / \\    \\");
+		System.out.println("     3   1   9  5  14   7");
+		
+		//l0
+		BinaryTree root = new BinaryTree(1);
+		
+		//l1
+		root.left = new BinaryTree(2);
+		root.right = new BinaryTree(3);
+		
+		//l2
+		root.left.left = new BinaryTree(4);
+		root.left.right = new BinaryTree(5);
+		root.right.left = new BinaryTree(10);
+		root.right.right = new BinaryTree(11);
+		
+		//l3
+		root.left.left.left = new BinaryTree(3);
+		root.left.right.left = new BinaryTree(1);
+		root.left.right.right = new BinaryTree(9);
+		root.right.left.left = new BinaryTree(5);
+		root.right.left.right = new BinaryTree(14);
+		root.right.right.left = new BinaryTree(7);
+
+		List<Integer> branchSums = branchSums(root);
+		System.out.println("Sums:" + branchSums);
+	}
 }
--- algorithms.expert/src/FindClosestValueInBst_3.java
+++ algorithms.expert/src/FindClosestValueInBst_3.java
@@ -43,7 +43,7 @@ class FindClosestValueInBst_3 {
         root.left.left.left = new BST(1);
         root.left.right = new BST(5);
         root.right = new BST(15);
-        root.right.left = new BST(13);ex
+        root.right.left = new BST(13);
         root.right.left.right = new BST(14);
         root.right.right = new BST(22);
 
